7471b07497843f96380d6123623aa93ee2b5d9f6
Jesse Pimenta <drrrreams@gmail.com>
2016-08-03 00:59:05 -0600

    added css files for homepage

---
+++ app.js
@@ -0,0 +1,22 @@
+app = angular.module( 'app', ['ui.router']);
+
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+$urlRouterProvider.otherwise('/home');
+
+$stateProvider
+
+    .state( 'home', {
+      url: 'home',
+      templateUrl: 'templates/home.html'
+    })
+    .state( '/about', {
+        url: 'about',
+      templateUrl: 'templates/login.html'
+    })
+    .state( '/FAQ', {
+        url:'FAQ',
+      templateUrl: 'templates/FAQ.html'
+    });
+  });
